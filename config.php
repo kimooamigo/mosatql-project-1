ca529d8369d33c8bd8ef6ce28041cb6454f3c814
Kareem Amged <85658103+kimooamigo@users.noreply.github.com>
2023-04-10 03:55:29 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+//fill these variables with your data
+//for more information about this file, visit: 
+//https://github.com/kleeja-official/kleeja/wiki/config.php-file
+
+$dbserver = 'localhost'; //database server 
+$dbuser   = 'u580176836_kleejaG' ; // database user 
+$dbpass   = 'Kleeja@2023'; // database password 
+$dbname   = 'u580176836_kleejaG'; // database name 
+$dbprefix = 'klj_'; // if you use prefix for tables , fill it 
